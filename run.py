052629798f683d1715408647b9dc60030ab67812
tetanushamster <armon.dressler@gmail.com>
2019-06-10 23:37:15 +0200

    changed data store from list to dict to store extra information

--- run.py
+++ run.py
@@ -15,18 +15,24 @@ class NhanesReader:
         return pd.read_csv(open(self.csvfile, "rb"), sep=",", index_col=1)
 
 
+csv_base_path = "data"
+csv_time_ranges = ["2013_2014"]
 sections = ["demographic", "diet", "examination", "labs", "medications", "questionnaire"]
-csv_path = "data/2013_2014"
 data_suffix = "csv"
 index_suffix = "idx"
-nhanes_data = {key: {} for key in sections}
+
+nhanes_data = {key: {} for key in csv_time_ranges}
+for timerange in csv_time_ranges:
+    nhanes_data[timerange] = {key: {} for key in sections}
+
+for timerange in nhanes_data.keys():
+    for section in nhanes_data[timerange].keys():
+        dataframe = NhanesReader(path.join(csv_base_path, timerange, section + "." + data_suffix), ",")
+        dataframe.data.set_index("SEQN")
+        nhanes_data[timerange][section] = dataframe
 
 
-for section in sections:
-    dataframe = NhanesReader(path.join(csv_path, section + "." + data_suffix), ",")
-    dataframe.data.set_index("SEQN")
-    nhanes_data[section] = (dataframe)
-
-
-for data in nhanes_data.values():
-    print(data.data.shape)
+for timerange in nhanes_data.keys():
+    print("Using timerange {}".format(timerange))
+    for section in nhanes_data[timerange].keys():
+        print(nhanes_data[timerange][section].data.shape)
