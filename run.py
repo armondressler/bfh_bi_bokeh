f5bb999983d52e7875e17d7e3bf48519fa0c4536
tetanushamster <armon.dressler@gmail.com>
2019-06-10 22:56:53 +0200

    moved datafiles

--- run.py
+++ run.py
@@ -4,25 +4,28 @@ from bokeh.plotting import figure, output_file, show
 from os import path
 
 
-
 class nhanes_data:
-    def __init__(self, datafile, delimiter):
-        self.datafile = datafile
+    def __init__(self, csvfile, delimiter):
+        self.csvfile = csvfile
         self.delimiter = delimiter
         self.data = self._load_file()
 
     def _load_file(self):
-        return pd.read_csv(open(self.datafile, "rb"), sep=",", index_col=1)
+        return pd.read_csv(open(self.csvfile, "rb"), sep=",", index_col=1)
 
 
 sections = ["demographic", "diet", "examination", "labs", "medications", "questionnaire"]
+csv_path = "data/2013_2014"
 data_suffix = "csv"
 index_suffix = "idx"
 datalist = []
 
 
 for section in sections:
-    datalist.append(nhanes_data(path.join("data/", section + "." + data_suffix), ","))
+    dataframe = nhanes_data(path.join(csv_path, section + "." + data_suffix), ",")
+    dataframe.data.set_index("SEQN")
+    datalist.append(dataframe)
+
 
 
 for data in datalist:
