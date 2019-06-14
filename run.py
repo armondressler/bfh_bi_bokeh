2c3bc01506ec4f76f91910675efda9cc9b131525
Armon <armon.dressler@gmail.com>
2019-06-14 21:18:20 +0200

    .

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import ColumnDataSource
+from bokeh.layouts import column
 from os import path
 
 
@@ -44,18 +44,36 @@ for timerange in nhanes_data.keys():
 #         print("summary:\n{}".format(nhanes_data[timerange][section].data.describe()))
 
 #source = ColumnDataSource(data=nhanes_data["2013_2014"]["questionnaire"].data)
-plot = figure()
-# timerange_of_interest = "2007_2008"
-# columns_of_interest = ["DPQ0"+str(count) for count in range(10,61,10)]
-# for variable in columns_of_interest:
-#     print("Section {}".format(variable))
-#     df = nhanes_data[timerange_of_interest]["questionnaire"].data[variable]
-#     df.dropna(inplace=True)
-#     for unique_val in sorted(df.unique()):
-#         print("Counted {} in {} cases.".format(unique_val,len(df.loc[df == unique_val])))
-#     print()
 
+timerange_of_interest = "2007_2008"
+columns_of_interest = ["DPQ0"+str(count) for count in range(10,61,10)]
+for variable in columns_of_interest:
+    print("Section {}".format(variable))
+    df = nhanes_data[timerange_of_interest]["questionnaire"].data[variable]
+    df.dropna(inplace=True)
+    for unique_val in sorted(df.unique()):
+        print("Counted {} in {} cases.".format(unique_val,len(df.loc[df == unique_val])))
+    print()
 
+###### Depression by Age
+df_q = nhanes_data["2013_2014"]["questionnaire"].data
+df_d = nhanes_data["2013_2014"]["demographic"].data
+df = pd.concat([df_q,df_d], axis=1)
+df = df[["DMDHRAGE", "DPQ010"]]
+df.dropna(inplace=True)
+df = df.loc[df["DPQ010"] == 3]
+
+bracketed_list = []
+for agebracket in range(20,81,10):
+    columns_of_interest = 0
+    for index,person in df.iterrows():
+        bracketed_list.append(person["DMDHRAGE"])
+
+hist, bins = np.histogram(bracketed_list, bins = [20,30,40,50,60,70,80])
+plot1 = figure(title="Depression Symptoms by Age (DPQ010)", tools='')
+plot1.quad(top=hist, bottom=0, left=bins[:-1], right=bins[1:],line_width=0.8)
+
+###### Depression by GENDER
 df_q = nhanes_data["2013_2014"]["questionnaire"].data
 df_d = nhanes_data["2013_2014"]["demographic"].data
 df = pd.concat([df_q,df_d], axis=1)
@@ -64,14 +82,26 @@ df.dropna(inplace=True)
 df = df.loc[df["DPQ090"] == 3]
 
 bracketed_list = []
-
-for index,person in df.iterrows():
+for index, person in df.iterrows():
     bracketed_list.append(person["RIAGENDR"])
 
 hist, bins = np.histogram(bracketed_list)
-p = figure(title="Depression Symptoms by Gender", tools='')
-p.quad(top=hist, bottom=0, left=bins[:-1], right=bins[1:])
-show(p)
+plot2 = figure(title="Depression Symptoms by Gender (DPQ090)", tools='')
+plot2.quad(top=hist, bottom=0, left=bins[:-1], right=bins[1:])
+
+
+
+
+
+
+
+
+
+
+
+bokeh_column = column(plot1,plot2, plot3)
+
+show(bokeh_column)
 
 
 
